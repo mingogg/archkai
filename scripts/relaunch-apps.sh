a1e605cd4354f8ed41772217d59714a613019775
mingogg <mingogadeapy@gmail.com>
2026-01-31 18:00:52 -0300

    add: relaunch mako too

--- scripts/relaunch-apps.sh
+++ scripts/relaunch-apps.sh
@@ -11,13 +11,19 @@ source ~/.bashrc
 # Wallpaper
 pkill swaybg
 sleep 0.1
-nohup swaybg -i "$CURRENT/wallpaper/wallpaper.jpg" -m fill >/dev/null 2>&1 &
+nohup swaybg -i "$CURRENT/wallpaper/wpp.jpg" -m fill >/dev/null 2>&1 &
 disown
 
 # Waybar
 pkill waybar
 sleep 0.1
 nohup waybar >/dev/null 2>&1 &
+disown
+
+# Mako notifications
+pkill mako
+sleep 0.1
+nohup mako >/dev/null 2>&1 &
 disown
 
 # Ly login manager service will update the theme after rebooting the system
