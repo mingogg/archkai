d6a6ea3f557e5db8b41935ea99fd783d623d1976
mingogg <mingogadeapy@gmail.com>
2026-02-02 18:06:21 -0300

    update the timeout to 1300

--- config/nvim/lua/config/plugins/noice.lua
+++ config/nvim/lua/config/plugins/noice.lua
@@ -12,6 +12,13 @@ return {
 					["cmp.entry.get_documentation"] = true,
 				},
 			},
+			routes = {
+				{
+					filter = { event = "msg_show" },
+					view = "notify",
+					opts = { timeout = 1300 },
+				},
+			},
 			presets = {
 				bottom_search = true,
 				command_palette = true,
@@ -20,9 +27,20 @@ return {
 				lsp_doc_border = true,
 			},
 		},
+		keys = {
+			{ "<leader>nl", function() require("noice").cmd("last") end, desc = "Último mensaje de Noice" },
+			{ "<leader>nh", function() require("noice").cmd("history") end, desc = "Historial de Noice" },
+			{ "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Cerrar notificaciones" },
+		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
+			{
+				"rcarriga/nvim-notify",
+				opts = {
+					timeout = 1300,
+					stages = "static",
+				},
+			},
 		},
 	},
 }
