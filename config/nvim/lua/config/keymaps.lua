74a8003cf8930b942fb75e2b7a2b678824ab2039
mingogg <mingogadeapy@gmail.com>
2026-07-10 23:38:59 -0300

    rm: telescope keybinds that points to my personal folders (is no use for default users that does not have those folders)

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -12,22 +12,12 @@ vim.keymap.set("n", "<down>", '<cmd>echo "USE j TO MOVE"<CR>')
 
 vim.keymap.set("n", "<S-Tab>", ":bnext<CR>", { desc = "Next buffer" })
 
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
-vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
-vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
-vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
-vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
-
 ---------- RUN PYTHON FILE ---------- 
 vim.keymap.set("n", "<leader>p", function()
 	vim.cmd("write")
 	local file = vim.fn.expand("%")
 	vim.cmd("split | terminal python3 " .. file)
-end, { desc = "Run current file with Python" })
+end, { desc = "Run Python file" })
 
 ---------- SURROUND LIST WITH "" CMD ---------- 
 -- Mark your list with 'vi{' and <leader>' to add "" to the items inside
@@ -65,23 +55,7 @@ vim.keymap.set("n", "<leader>fc", function()
 		cwd = vim.fn.expand("~/.config"),
 		hidden = true,
 	})
-end, { desc = "Find in CONFIG" })
-
--- CMD TO SEARCH INSIDE HOME FOLDER
-vim.keymap.set("n", "<leader>fh", function()
-	require("telescope.builtin").find_files({
-		cwd = vim.fn.expand("~/"),
-		hidden = true,
-	})
-end, { desc = "Find in HOME" })
-
--- CMD TO SEARCH INSIDE DEV FOLDER
-vim.keymap.set("n", "<leader>fd", function()
-	require("telescope.builtin").find_files({
-		cwd = vim.fn.expand("~/dev"),
-		hidden = true,
-	})
-end, { desc = "Find in DEV" })
+end, { desc = "Find in .config" })
 ---------- END TELESCOPE CMDS ----------
 
 ---------- START MINI-SURROUND CMDS ----------
